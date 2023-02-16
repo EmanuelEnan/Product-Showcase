c05182f3a3ba2e83c65accb19eeb8dab453c1ddd
EmanuelEnan <emaanuelenan@gmail.com>
2023-02-16 19:36:26 +0600

    new commit3

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_network/image_network.dart';
+import 'package:product_showcase/cubit/product_cubit.dart';
 
 import '../repos/product_repo.dart';
 
@@ -9,46 +13,80 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final productRepo = ProductRepo();
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder(
-              future: productRepo.getProducts(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.data.products.results.length,
-                    itemBuilder: (context, index) {
-                      return Card(
-                        child: Column(
-                          children: [
-                            Text(
-                              snapshot.data!.data.products.results[index].slug,
-                            ),
-                            Text(
-                              snapshot.data!.data.products.results[index]
-                                  .productName,
-                            ),
-                            Text(
-                              snapshot
-                                  .data!.data.products.results[index].seller,
-                            ),
-                          ],
-                        ),
+      body: BlocBuilder<ProductCubit, ProductState>(
+        builder: (context, state) {
+          return Column(
+            children: [
+              Expanded(
+                child: FutureBuilder(
+                  future: productRepo.getProducts(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
                       );
-                    },
-                  );
-                } else {
-                  return const Text('there are some errors');
-                }
-              },
-            ),
-          ),
-        ],
+                    } else if (snapshot.hasData) {
+                      return GridView.builder(
+                        itemCount: snapshot.data!.data.products.results.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 4.0,
+                          mainAxisSpacing: 4.0,
+                        ),
+                        itemBuilder: (context, index) {
+                          return Container(
+                            padding: const EdgeInsets.all(8),
+                            height: MediaQuery.of(context).size.height * .65,
+                            child: Card(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  ImageNetwork(
+                                    image: snapshot.data!.data.products
+                                        .results[index].image,
+                                    height:
+                                        MediaQuery.of(context).size.height * .2,
+                                    width:
+                                        MediaQuery.of(context).size.width * .2,
+                                  ),
+                                  Text(
+                                    snapshot.data!.data.products.results[index]
+                                        .productName,
+                                    overflow: TextOverflow.fade,
+                                    style: GoogleFonts.notoSansBengali(),
+                                  ),
+                                  Text(
+                                    'buy: ${snapshot.data!.data.products.results[index].charge.currentCharge.toString()}',
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'sell: ${snapshot.data!.data.products.results[index].charge.sellingPrice.toString()}',
+                                      ),
+                                      Text(
+                                        'gain: ${(snapshot.data!.data.products.results[index].charge.sellingPrice - snapshot.data!.data.products.results[index].charge.currentCharge)}'
+                                            .toString(),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const Text('there are some errors');
+                    }
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
